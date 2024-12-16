4b21de25d32abc7ae4534686b3c144911e57333c
Trevor Brown <admin@stratus3d.com>
2024-12-16 13:39:22 -0500

    fixup! feat(golang-rewrite): remove `asdf update` command to prepare for Go version

--- lib/commands/command-update.bash
+++ lib/commands/command-update.bash
@@ -1,12 +1,7 @@
 # -*- sh -*-
 
 update_command() {
-  printf "Upgrading asdf via asdf update is no longer supported. Please use your OS
-package manager (Homebrew, APT, etc...) to upgrade asdf or download the
-latest asdf binary manually from the asdf website.
-
-Please visit https://asdf-vm.com/ or https://github.com/asdf-vm/asdf for more
-details.\n"
+  printf "Upgrading asdf via asdf update is no longer supported. Please use your OS\npackage manager (Homebrew, APT, etc...) to upgrade asdf or download the\nlatest asdf binary manually from the asdf website.\n\nPlease visit https://asdf-vm.com/ or https://github.com/asdf-vm/asdf for more\ndetails.\n"
 
   exit 1
 }
